70a342f05c5b7f2e7269a78fe958473524cecadc
Will Hernandez <w.hernandez.code@gmail.com>
2023-09-07 09:55:14 -0400

    post build,req body  updated

--- src/Header.js
+++ src/Header.js
@@ -45,11 +45,14 @@ const crossOriginDownload = href => event => {
 
 const deleteImage = href => event => {
   event.preventDefault();
+  const username = localStorage.getItem('username')
+  const accesstoken = localStorage.getItem('accesstoken')
+
   const req = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ urls: [href] })
-};
+    body: JSON.stringify({ urls: [href], username, accesstoken })
+  };
 
   // fetch('https://file-storage-backend-original.onrender.com/api/bucket/delete', req)
   fetch('http://localhost:4000/api/bucket/delete', req)
